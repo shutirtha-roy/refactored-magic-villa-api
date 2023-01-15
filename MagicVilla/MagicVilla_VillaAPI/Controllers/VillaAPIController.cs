fdba3816a654064fcd0c7b665bae05373e58ca14
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-15 15:58:37 +0600

    02-First End Point

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,8 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
---
+++ MagicVilla/MagicVilla_VillaAPI/Model/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Model
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}

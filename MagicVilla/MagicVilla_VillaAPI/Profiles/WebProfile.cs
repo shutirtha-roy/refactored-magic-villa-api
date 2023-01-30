5bb9bd635b37b21d12053a80f1a35a8afab2802d
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-30 14:09:45 +0600

    VillaProfile mapping added

---
+++ MagicVilla/MagicVilla_VillaAPI/Profiles/WebProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Model;
+using VillaBO = MagicVilla_Infrastructure.BusinessObjects.Villa;
+
+namespace MagicVilla_VillaAPI.Profiles
+{
+    public class WebProfile : Profile
+    {
+        public WebProfile()
+        {
+            CreateMap<VillaCreateModel, VillaBO>()
+                .ReverseMap();
+        }
+    }
+}

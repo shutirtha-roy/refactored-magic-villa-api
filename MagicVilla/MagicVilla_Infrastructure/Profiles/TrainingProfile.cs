aafce86757046dd47c88192ad9f936e6266643bb
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-30 14:05:09 +0600

    TrainingProfile Added for mapping

---
+++ MagicVilla/MagicVilla_Infrastructure/Profiles/TrainingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using VillaBO = MagicVilla_Infrastructure.BusinessObjects.Villa;
+using VillaEO = MagicVilla_Infrastructure.Entities.Villa;
+
+
+namespace MagicVilla_Infrastructure.Profiles
+{
+    public class TrainingProfile : Profile
+    {
+        public TrainingProfile()
+        {
+            CreateMap<VillaEO, VillaBO>()
+                .ReverseMap();
+        }
+    }
+}

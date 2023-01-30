72cb712da86914187b88d49ec2939436f9b8ba51
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-30 12:44:53 +0600

    Villa Method Signature Added

---
+++ MagicVilla/MagicVilla_Infrastructure/Services/IVillaService.cs
@@ -0,0 +1,14 @@
+﻿using VillaBO = MagicVilla_Infrastructure.BusinessObjects.Villa;
+using VillaEO = MagicVilla_Infrastructure.Entities.Villa;
+
+namespace MagicVilla_Infrastructure.Services
+{
+    public interface IVillaService
+    {
+        Task CreateVilla();
+        Task EditVilla(VillaEO villa);
+        Task DeleteVilla();
+        Task<VillaBO> GetVilla(int id);
+        Task<IList<VillaBO>> GetVillas();
+    }
+}

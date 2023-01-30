03bb864e3463ecffaa2f6a3ed656e009793df4a2
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-30 14:12:24 +0600

    VillaCreateModel Implemented

---
+++ MagicVilla/MagicVilla_VillaAPI/Model/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace MagicVilla_VillaAPI.Model
+{
+    public class BaseModel
+    {
+        protected ILifetimeScope _scope;
+
+        public BaseModel()
+        {
+
+        }
+
+        public virtual void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+    }
+}

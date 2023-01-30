04a243deb887ee023682d59bd7a6f274fb4f68e0
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-01-30 10:30:19 +0600

    VillaRepository Implemented

---
+++ MagicVilla/MagicVilla_Infrastructure/Repositories/IVillaRepository.cs
@@ -0,0 +1,8 @@
+﻿using MagicVilla_Infrastructure.Entities;
+
+namespace MagicVilla_Infrastructure.Repositories
+{
+    public interface IVillaRepository : IRepository<Villa, int>
+    {
+    }
+}
---
+++ MagicVilla/MagicVilla_Infrastructure/Repositories/VillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_Infrastructure.DbContexts;
+using MagicVilla_Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_Infrastructure.Repositories
+{
+    public class VillaRepository : Repository<Villa, int>, IVillaRepository
+    {
+        public VillaRepository(IApplicationDbContext context) : base((DbContext)context)
+        {
+
+        }
+    }
+}

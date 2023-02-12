de781a364a1d62356cea30ccd95c626c8386ab22
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-13 05:07:43 +0600

    VillaNumber Entity and Business Object added for mapping

--- MagicVilla/MagicVilla_Infrastructure/Profiles/TrainingProfile.cs
+++ MagicVilla/MagicVilla_Infrastructure/Profiles/TrainingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using VillaBO = MagicVilla_Infrastructure.BusinessObjects.Villa;
 using VillaEO = MagicVilla_Infrastructure.Entities.Villa;
+using VillaNumberBO = MagicVilla_Infrastructure.BusinessObjects.VillaNumber;
+using VillaNumberEO = MagicVilla_Infrastructure.Entities.VillaNumber;
 
 
 namespace MagicVilla_Infrastructure.Profiles
@@ -10,6 +12,9 @@ namespace MagicVilla_Infrastructure.Profiles
         public TrainingProfile()
         {
             CreateMap<VillaEO, VillaBO>()
+                .ReverseMap();
+
+            CreateMap<VillaNumberEO, VillaNumberBO>()
                 .ReverseMap();
         }
     }

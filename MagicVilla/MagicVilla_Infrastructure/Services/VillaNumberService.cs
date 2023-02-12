80131822ad5d2934ec1d4079e1e8a473dd58e316
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-13 05:06:19 +0600

    Methods added

--- MagicVilla/MagicVilla_Infrastructure/Services/VillaNumberService.cs
+++ MagicVilla/MagicVilla_Infrastructure/Services/VillaNumberService.cs
@@ -1,6 +1,45 @@
-﻿namespace MagicVilla_Infrastructure.Services
+﻿using AutoMapper;
+using MagicVilla_Infrastructure.BusinessObjects;
+using MagicVilla_Infrastructure.UnitOfWorks;
+using VillaNumberBO = MagicVilla_Infrastructure.BusinessObjects.VillaNumber;
+using VillaNumberEO = MagicVilla_Infrastructure.Entities.VillaNumber;
+
+namespace MagicVilla_Infrastructure.Services
 {
     public class VillaNumberService : IVillaNumberService
     {
+        private readonly IApplicationUnitOfWork _applicationUnitOfWork;
+        private readonly IMapper _mapper;
+
+        public VillaNumberService(IApplicationUnitOfWork applicationUnitOfWork, IMapper mapper)
+        {
+            _applicationUnitOfWork = applicationUnitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task CreateVillaNumber(VillaNumberBO villaNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteVillaNumber(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task EditVillaNumber(VillaNumberBO villaNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<VillaNumberBO> GetVillaNumber(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<VillaNumberBO>> GetVillaNumbers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

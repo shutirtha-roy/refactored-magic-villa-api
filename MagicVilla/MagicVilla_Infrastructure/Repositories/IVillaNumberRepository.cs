8737250ffa5aca6e301359161c436682429e35ff
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-13 19:52:59 +0600

    GetVillaNumberById Implemented

--- MagicVilla/MagicVilla_Infrastructure/Repositories/IVillaNumberRepository.cs
+++ MagicVilla/MagicVilla_Infrastructure/Repositories/IVillaNumberRepository.cs
@@ -4,5 +4,6 @@ namespace MagicVilla_Infrastructure.Repositories
 {
     public interface IVillaNumberRepository : IRepository<VillaNumber, int>
     {
+        Task<VillaNumber> GetVillaNumberById(int VillaNo);
     }
 }
--- MagicVilla/MagicVilla_Infrastructure/Repositories/VillaNumberRepository.cs
+++ MagicVilla/MagicVilla_Infrastructure/Repositories/VillaNumberRepository.cs
@@ -6,9 +6,16 @@ namespace MagicVilla_Infrastructure.Repositories
 {
     public class VillaNumberRepository : Repository<VillaNumber, int>, IVillaNumberRepository
     {
+        private readonly IApplicationDbContext _dbContext;
+
         public VillaNumberRepository(IApplicationDbContext context) : base((DbContext)context)
         {
+            _dbContext = context;
+        }
 
+        public async Task<VillaNumber> GetVillaNumberById(int villaNo)
+        {
+            return _dbContext.VillaNumbers.Include(x => x.Villa).Where(y => y.VillaNo == villaNo).FirstOrDefault();
         }
     }
 }

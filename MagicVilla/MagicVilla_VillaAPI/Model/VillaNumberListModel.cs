51bbcf12e7c3e39119323b7aa219ecc9040414f8
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-13 20:05:34 +0600

    VillaNumberListModel Implemented

---
+++ MagicVilla/MagicVilla_VillaAPI/Model/VillaNumberListModel.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using MagicVilla_Infrastructure.Services;
+using VillaNumberBO = MagicVilla_Infrastructure.BusinessObjects.VillaNumber;
+
+namespace MagicVilla_VillaAPI.Model
+{
+    public class VillaNumberListModel : BaseModel
+    {
+        private IVillaNumberService _villaNumberService;
+
+        public VillaNumberListModel(IVillaNumberService villaNumberService)
+        {
+            _villaNumberService = villaNumberService;
+        }
+
+        public override void ResolveDependency(ILifetimeScope scope)
+        {
+            base.ResolveDependency(scope);
+            _villaNumberService = _scope.Resolve<IVillaNumberService>();
+        }
+
+        internal async Task<IList<VillaNumberBO>> GetAllVillaNumbers()
+        {
+            var villaNumbers = await _villaNumberService.GetVillaNumbers();
+            return villaNumbers;
+        }
+
+        internal async Task DeleteVillaNumber(int villaNo)
+        {
+            await _villaNumberService.DeleteVillaNumber(villaNo);
+        }
+    }
+}

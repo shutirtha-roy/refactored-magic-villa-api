90d49f2357ac4b5f6e6656fd6ba9b600939cdc98
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-14 11:16:24 +0600

    VillaNumberAPIController Added

---
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaNumberAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using MagicVilla_VillaAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaNumberAPIController : Controller
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly APIResponse _response;
+
+        public VillaNumberAPIController(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _response = _scope.Resolve<APIResponse>();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

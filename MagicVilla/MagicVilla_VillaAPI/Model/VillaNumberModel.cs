ee858c98870e41db121271e58d97f7337d732fff
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-14 23:35:31 +0600

    VillaNumberModel Implemented

---
+++ MagicVilla/MagicVilla_VillaAPI/Model/VillaNumberModel.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_Infrastructure.BusinessObjects;
+
+namespace MagicVilla_VillaAPI.Model
+{
+    public class VillaNumberModel : BaseModel
+    {
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        public int VillaId { get; set; }
+        public Villa Villa { get; set; }
+    }
+}

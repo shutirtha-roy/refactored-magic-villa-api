bbacfd48dd276619c89b53fa14ae940baac85480
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-17 16:58:59 +0600

    IVillaService Implemented

---
+++ MagicVilla/MagicVilla_Web/Services/IService/IVillaService.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_Web.Model;
+
+namespace MagicVilla_Web.Services.IService
+{
+    public interface IVillaService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> CreateAsync<T>(VillaCreateModel model);
+        Task<T> UpdateAsync<T>(VillaEditModel model);
+        Task<T> DeleteAsync<T>(int id);
+    }
+}

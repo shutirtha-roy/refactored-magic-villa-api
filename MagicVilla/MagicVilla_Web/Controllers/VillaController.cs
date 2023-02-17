2656403f44418b409345c82980b26c1e8cedb90d
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-17 20:33:13 +0600

    VillaController Implemented

---
+++ MagicVilla/MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            var villaList = new List<VillaModel>();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+
+            if(response != null && response.IsSuccess)
+            {
+                villaList = JsonConvert.DeserializeObject<List<VillaModel>>(Convert.ToString(response.Result));
+            }
+
+            return View(villaList);
+        }
+    }
+}

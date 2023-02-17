692212e1c3d865eba622dd7c8edafca77f966298
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-02-17 06:12:55 +0600

    WebProfile Added

---
+++ MagicVilla/MagicVilla_Web/Profiles/WebProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Model;
+using VillaBO = MagicVilla_Infrastructure.BusinessObjects.Villa;
+using VillaNumberBO = MagicVilla_Infrastructure.BusinessObjects.VillaNumber;
+
+namespace MagicVilla_Web.Profiles
+{
+    public class WebProfile : Profile
+    {
+        public WebProfile()
+        {
+            CreateMap<VillaCreateModel, VillaBO>()
+                .ReverseMap();
+
+            CreateMap<VillaBO, VillaEditModel>()
+                .ReverseMap();
+
+            CreateMap<VillaNumberCreateModel, VillaNumberBO>()
+                .ReverseMap();
+
+            CreateMap<VillaNumberBO, VillaNumberEditModel>()
+                .ReverseMap();
+
+            CreateMap<VillaNumberBO, VillaNumberModel>()
+                .ReverseMap();
+        }
+    }
+}
